751db0b14693226fab61147ba4e3ce0a9f1af24a
Kris0011 <krishp759@gmail.com>
2024-08-10 22:39:01 +0530

    socket for bidding

--- server/src/Routes/AuctionRouter.js
+++ server/src/Routes/AuctionRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {getAuctions ,createAuction} from '../controllers/Auction.js';
+import {getAuctions ,createAuction, getAuctionById} from '../controllers/Auction.js';
 import { isAuthenticated } from '../Middleware/IsAuthinticated.js';
 
 const AuctionRouter= express.Router();
 AuctionRouter.route('/auctions').get(getAuctions);
 AuctionRouter.route('/createauctions').post(isAuthenticated,createAuction);
+AuctionRouter.route('/auction').post(isAuthenticated,getAuctionById);
 
 
 export default AuctionRouter;
